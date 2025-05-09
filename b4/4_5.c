8a322d67e3115ed9d44115378a6a627f981c1e3c
ltkdt <kienlt2306@gmail.com>
2025-05-09 21:56:15 +0700

    full bai 4

--- b4/4_5.c
+++ b4/4_5.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <math.h>
 
+/*
+void swap(double *a, double *b){
+    double t = *a;
+    *a = *b;
+    *b = t;
+}
+*/
+
 int main(){
     double a, b, c;
     scanf("%lf", &a);
@@ -11,9 +19,31 @@ int main(){
         if ( b == 0 && c == 0){
             printf("Phuong trinh vo so nghiem");
         }
-        if ( b == 0 && c != 0){
+        else if ( b == 0 && c != 0){
             printf("Phuong trinh vo nghiem");
         }
-        printf("%lf", -c / b);
+        else{
+            printf("%lf", -c / b);
+        }
     }
+    else{
+        double r1, r2, re, im,  d = b * b - 4 * a * c;
+        if(d > 0 ){
+            r1 = (-b + sqrt(d)) / (2 * a);
+            r2 = (-b - sqrt(d)) / (2 * a);
+            printf("%lf\n", r1);
+            printf("%lf", r2);
+        }
+        else if (d == 0 ){
+            r1 = r2 = -b / (2 * a);
+            printf("%lf", r1);
+        }
+        else{
+            re = -b / (2 * a);
+            im = sqrt(-d) / (2 * a);
+            printf("%lf+%lfi\n", re, im);
+            printf("%lf-%lfi", re, im);
+        }
+    }
+
 }
