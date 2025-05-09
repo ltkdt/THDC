fe903352fc034b99588751ed7528cb391fd36af8
ltkdt <kienlt2306@gmail.com>
2025-05-09 20:53:25 +0700

    4.7

---
+++ b4/4_7.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int n;
+    double x, out = 0;
+
+    scanf("%d", &n);
+    scanf("%lf", &x);
+
+    if(n > 0){
+        while( n >= 0){
+            out += pow(-1, n) * pow(x, n);
+            n--;
+        }
+        printf("%lf", out);
+    }
+    else{
+        printf("Error");
+    }
+}
